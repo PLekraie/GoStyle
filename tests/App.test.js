eeda804fa5d30f2372eb0a02709b108f995bd6fd
lenaraa <mara.berlow@gmail.com>
2021-04-17 13:55:32 +0200

    test(global):rebase

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,13 +1,42 @@
-import { render, fireEvent } from '@testing-library/react-native';
-const { default: App } = require('../App');
-//const app = require('./App');
+/* eslint-disable no-undef */
+import 'react-native';
+import React from 'react';
+import {fireEvent, render} from '@testing-library/react-native';
+import App from '../App';
+import {expect, it} from '@jest/globals';
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
-  });
 
-test('App works', () => {
-    const mockFn = jest.fn();
-    const testz = []
+test('the app renders', async () => {
+  await expect(render(<App />).toJSON()).toBeDefined();
+});
+
+/*test('bouton voir promotion', async () => {
+
+  //recuperer le bouton
+  const button = render(<App>Voir mes promotions</App>);
   
+  //recuperer la valeur de modalevisible
+  const modal = render(<App>modalVisible</App>)
+
+  //ça doit être égal
+  //await expect(button).toEqual(modal);
+
+  //appuyer sur le bouton
+  //fireEvent.press(button);
+
+  //ça doit être différent
+  await expect(button).not.toEqual(modal);
+});*/
+
+/*
+describe('test App', () => {
+  it('bouton voir promotion', () =>{
+    //const push = jest.fn();
+    const { getByText } = render(<App>Voir mes promotions</App>);
+    const innerText = getByText(/press/i);
+    const button = innerText.parent as ReactTestInstance;
+    fireEvent.press(getByText(/press/i));
+    expect(push).toBeCalled();
   });
+});
+*/
